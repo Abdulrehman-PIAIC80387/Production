64a79310334870de5c7da3c77ae4411f40171899
Rehman <mirza.abdulrehman.336@gmail.com>
2021-09-04 11:18:00 +0500

    push text files

--- weather/settings.py
+++ weather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v^986l9(gd65_=w@pljukujxydthympbutejn%o%p9omt2^^=u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://theta-solutions.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
