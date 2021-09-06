89bc15e7107c23598e91958ce97854e0dfffff67
Rehman <mirza.abdulrehman.336@gmail.com>
2021-09-06 16:46:15 +0500

    add

--- weather/settings.py
+++ weather/settings.py
@@ -125,11 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
-staticfile_dir=[
-
-os.path.join(BASE_DIR, 'static')
-
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -262,11 +258,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
-staticfile_dir=[
 
-os.path.join(BASE_DIR, 'static')
-
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
